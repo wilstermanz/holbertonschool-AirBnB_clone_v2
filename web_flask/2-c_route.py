7653d1039b637168f9fd593c619f88ae67357183
AaronManuel15 <aaronmanuel15@gmail.com>
2022-11-11 10:40:32 -0800

    Task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Task 2: Script that starts a Flask web application"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_HBNB():
+    """Document"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def HBNB():
+    """Document"""
+    return "HBNB"
+
+
+@app.route("/c/<string:text>")
+def c_text(text):
+    """Document"""
+    text = text.replace("_", " ")
+    return f'C {text}'
+
+if __name__ == "__main__":
+    """Document"""
+    app.run(host="0.0.0.0", port="5000")
