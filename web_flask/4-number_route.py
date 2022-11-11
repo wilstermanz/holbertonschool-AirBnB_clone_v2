ef64a81339226a4165b122a2c255a30bd207de0c
AaronManuel15 <aaronmanuel15@gmail.com>
2022-11-11 12:41:40 -0800

    Task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Task 4: Script that starts a Flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_HBNB():
+    """Document"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def HBNB():
+    """Document"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    """Document"""
+    text = text.replace("_", " ")
+    return ("C {}".format(text))
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def python_text(text="is cool"):
+    """Document"""
+    text = text.replace("_", " ")
+    return ("Python {}".format(text))
+
+
+@app.route("/number/<int:n>")
+def number_text(n):
+    """Document"""
+    return ("{} is a number".format(n))
+
+
+if __name__ == "__main__":
+    """Document"""
+    app.run(host="0.0.0.0", port="5000")
