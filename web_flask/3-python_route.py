0a2c0500172084ffb6b1db928ba2ecd0083134f6
wilstermanz <wilstermanz@gmail.com>
2022-11-12 15:44:43 -0600

    Solution for task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This module starts a Flask web application
+"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route("/hbnb")
+def hbnb():
+    return 'HBNB'
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    text = text.replace('_', ' ')
+    return "C {}".format(escape(text))
+
+
+@app.route("/python/<text>")
+def c_text(text="is cool"):
+    text = text.replace('_', ' ')
+    return "Python {}".format(escape(text))
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5000)
