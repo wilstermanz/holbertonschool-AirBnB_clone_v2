997f09169a94410d0e9718d9472b697304bf3292
wilstermanz <wilstermanz@gmail.com>
2022-11-15 16:51:33 -0600

    Solution for task 8

--- main.py
+++ main.py
@@ -5,29 +5,27 @@
 from models import storage
 from models.state import State
 from models.city import City
-from models.user import User
 
 """
  Objects creations
 """
 state_1 = State(name="California")
-print("New state: {}".format(state_1))
+print("***\nNew state: {}".format(state_1))
 state_1.save()
 state_2 = State(name="Arizona")
-print("New state: {}".format(state_2))
+print("***\nNew state: {}".format(state_2))
 state_2.save()
 
 city_1_1 = City(state_id=state_1.id, name="Napa")
-print("New city: {} in the state: {}".format(city_1_1, state_1))
+print("***\nNew city: {} in the state: {}".format(city_1_1, state_1))
 city_1_1.save()
 city_1_2 = City(state_id=state_1.id, name="Sonoma")
-print("New city: {} in the state: {}".format(city_1_2, state_1))
+print("***\nNew city: {} in the state: {}".format(city_1_2, state_1))
 city_1_2.save()
 city_2_1 = City(state_id=state_2.id, name="Page")
-print("New city: {} in the state: {}".format(city_2_1, state_2))
+print("***\nNew city: {} in the state: {}".format(city_2_1, state_2))
 city_2_1.save()
 
-user = User()
 
 """
  Verification
---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage, state
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown(content):
+    """Call close"""
+    storage.close()
+
+
+@app.route("/states_list")
+def states_list():
+    states = storage.all(state.State)
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5000)
