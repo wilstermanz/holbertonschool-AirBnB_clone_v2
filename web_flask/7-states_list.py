e28027ab7e6666723dae626e40f6c9fdc07f23b7
wilstermanz <wilstermanz@gmail.com>
2022-11-15 16:59:27 -0600

    trying to figure out to use teardown

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,16 +9,16 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.teardown_appcontext
-def teardown(content):
-    """Call close"""
-    storage.close()
-
-
 @app.route("/states_list")
 def states_list():
     states = storage.all(state.State)
     return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(states_list):
+    """Call close"""
+    storage.close()
 
 
 if __name__ == "__main__":
