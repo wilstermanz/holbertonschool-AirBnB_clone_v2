1d99eb9749a1d3afc4100d7022e25226dfb6589b
wilstermanz <wilstermanz@gmail.com>
2022-11-15 17:03:05 -0600

    fixed

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,25 @@
-
 #!/usr/bin/python3
 """
-something
+This module starts a Flask web application
 """
 from flask import Flask, render_template
+from models import storage, state
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.teardown_appcontext
-def teardown(content):
-    """ call close """
-    import models
-    models.storage.close()
-
-
-@app.route('/states_list')
+@app.route("/states_list")
 def states_list():
-    """ print list of states in html page """
-    import models
-    states = models.storage.all(models.state.State)
+    states = storage.all(state.State)
     return render_template("7-states_list.html", states=states)
 
 
+@app.teardown_appcontext
+def teardown(states_list):
+    """Call close"""
+    storage.close()
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run("0.0.0.0", 5000)
